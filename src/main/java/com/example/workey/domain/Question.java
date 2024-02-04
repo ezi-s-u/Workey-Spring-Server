96bf8b5ddbf8818e39a831bb2f1f33b3e66412d9
haeunNoh06 <okhagrace06@naver.com>
2024-02-04 22:57:18 +0900

    :sparkles: [feat] questions domain 완성

--- src/main/java/com/example/workey/domain/Question.java
+++ src/main/java/com/example/workey/domain/Question.java
@@ -1,4 +1,21 @@
 package com.example.workey.domain;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Setter
+@Getter
+@Builder
+@Table(name = "questions")
 public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "quesId", nullable = false, updatable = false)
+    private long id;
+
+    @Column(nullable = false)
+    private String question;
 }
