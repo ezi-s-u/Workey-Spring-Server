c412e13a1611c29bdab069c2ed8b252040e90489
haeunNoh06 <okhagrace06@naver.com>
2024-02-04 20:57:53 +0900

    Initial commit

---
+++ src/test/java/com/example/workey/WorkeySpringServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.workey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkeySpringServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
