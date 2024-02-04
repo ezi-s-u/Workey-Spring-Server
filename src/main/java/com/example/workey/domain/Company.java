3567edc85aa274178ac06bcd3c7dadbbb3f67592
haeunNoh06 <okhagrace06@naver.com>
2024-02-04 23:03:53 +0900

    :sparkles: [feat] companies domain 완성

--- src/main/java/com/example/workey/domain/Company.java
+++ src/main/java/com/example/workey/domain/Company.java
@@ -1,4 +1,28 @@
 package com.example.workey.domain;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Setter
+@Getter
+@Builder
+@Table(name = "companies")
 public class Company {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "companyId", nullable = false, updatable = false)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column
+    private String image;
+
+    @Column
+    private long totalGoodStateCount;
+
 }
